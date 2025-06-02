387ee2a36143f0b5f6a8853b1c7ea46057152ec5
DemonKoly <nikolasekmensik211@gmail.com>
2025-06-02 19:40:08 +0200

    Delete raah directory

--- raah/test.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("clickoff").addEventListener("click", function () {
-  document.getElementById("clickoff").style.display = "none";
-});
-document.getElementById("tlacidlo2").addEventListener("click", function () {
-  document.getElementById("clickoff").style.display = "flex";
-});
